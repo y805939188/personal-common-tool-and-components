cd5d265e6c395259a380a85035aafd81948a4bbd
Ding Yubo <dingyubo@4paradigm.com>
2019-12-17 14:17:13 +0800

    update unit  test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  collectCoverage: true,
   "transform": {
     "^.+\\.(t|j)sx?$": "ts-jest"
   },
--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,5 +1,59 @@
-import ding from '../index';
+import LoopPromise from '../';
 
-test("My Test", () => {
-  expect(ding).toBe(6666);
+describe('测试LoopPromise', () => {
+  test("Test LoopPromise", (cb) => {
+    const fn = jest.fn();
+    let index1 = 0;
+    let index2 = 0;
+    const TestPromise1 = () => (
+      new Promise((resolve) => {
+        resolve(++index1);
+      })
+    );
+    const TestPromise2 = () => (
+      new Promise((resolve) => {
+        fn();
+        resolve(++index2);
+      })
+    );
+    const TestPromise3 = () => (
+      new Promise((resolve, reject) => {
+        reject();
+      })
+    );
+    const loop1 = new LoopPromise(TestPromise1);
+    const loop2 = new LoopPromise(TestPromise2);
+    const loop3 = new LoopPromise(TestPromise3);
+    loop1
+      .time(1000)
+      .interruptTime(res => (res !== 3))
+      .params(() => (''))
+      .filterRes(res => res)
+      .callback(res => {})
+      .trim()
+    setTimeout(() => loop1.immediatelyInterrupt(true), 1500)
+    loop2
+      .time(1500)
+      .interruptTime(3, true)
+      .callback(res => {
+        if (res === 3) {
+          expect(fn).toBeCalledTimes(3);
+          loop2.clear();
+          cb();
+        }
+      })
+      .trim()
+    loop3
+      .time(1000)
+      .trim()
+
+    setTimeout(() => {
+      loop2.immediatelyInterrupt();
+      setTimeout(() => {
+        loop2.restart();
+      }, 1000)
+    }, 1000)
+    
+
+  }, 10000);
 });
